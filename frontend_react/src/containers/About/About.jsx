4c27586f0776366e544ad7b23101382851c31192
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-04-24 19:10:44 +0530

    Frontend Programming for About Section Almost Completed

--- frontend_react/src/containers/About/About.jsx
+++ frontend_react/src/containers/About/About.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {motion} from 'framer-motion';
 
+import {images} from '../../constants';
 import './About.scss';
+
+const about = [
+  {title : 'Web Development', description : 'I am a good Web Developer.', imgUrl : images.about01},
+  {title : 'Competitive Programming', description : 'I am a good Competitive Programming.', imgUrl : images.about02},
+  {title : 'Arduino Programming', description : 'I am a good Arduino Programmer.', imgUrl : images.about03},
+  {title : 'Date Structure and Algorithm', description : 'I know Date Structure and Algorithm very well.', imgUrl : images.about04},
+];
 
 const About = () => {
   return (
-    <h1>About</h1>
+    <>
+      <h2 className="head-text">I know that <span>Good Development</span><br/>means  <span>Good Business</span>.</h2>
+      <div className="app__profiles">
+        {about.map((element, index)=>{
+          return (
+            <motion.div
+              whileInView={{opacity : 1}}
+              whileHover={{scale: 1.1}}
+              transition={{duration : 0.5, type : 'tween'}}
+              className='app__profile-item'
+              key={element.title + index}
+            >
+              <img src={element.imgUrl} alt={element.title}/>
+              <h2 className="bold-text" style={{marginTop : 20}}>{element.title}</h2>
+              <p className="p-text" style={{marginTop : 10}}>{element.description}</p>
+            </motion.div>
+          );
+        })}
+      </div>
+    </>
   )
 }
 
